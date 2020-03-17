2ee5ea2f7dd64242689e36256cbff648e001777f
philcleveland <pdoh00@gmail.com>
2020-03-16 19:31:13 -0700

    init commit

---
+++ src/services/EventService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:3000',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getEvents() {
+        apiClient.get('/events')
+    },
+    getEvent(id){
+        apiClient.get('/events/' + id)
+    }
+
+}
